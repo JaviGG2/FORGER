c00fa8e538f35d569ecfce095205df91e151f448
JavierG <corotour55@gmail.com>
2026-09-02 21:26:07 -0400

    fix: remove unused Route import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Banner from './components/Banner'
